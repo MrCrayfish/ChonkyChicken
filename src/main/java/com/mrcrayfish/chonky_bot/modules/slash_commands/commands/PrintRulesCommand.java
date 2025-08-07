0c8c148a083cf18034d90de1f859bb62a41ad99c
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-08-08 05:36:39 +0930

    📦 Use new components v2 for rules

--- src/main/java/com/mrcrayfish/chonky_bot/modules/slash_commands/commands/PrintRulesCommand.java
+++ src/main/java/com/mrcrayfish/chonky_bot/modules/slash_commands/commands/PrintRulesCommand.java
@@ -3,6 +3,12 @@ package com.mrcrayfish.chonky_bot.modules.slash_commands.commands;
 import com.mrcrayfish.chonky_bot.Constants;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.components.container.Container;
+import net.dv8tion.jda.api.components.container.ContainerChildComponent;
+import net.dv8tion.jda.api.components.section.Section;
+import net.dv8tion.jda.api.components.separator.Separator;
+import net.dv8tion.jda.api.components.textdisplay.TextDisplay;
+import net.dv8tion.jda.api.components.thumbnail.Thumbnail;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -10,6 +16,7 @@ import net.dv8tion.jda.api.interactions.InteractionContextType;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,43 +38,29 @@ public class PrintRulesCommand extends SlashCommand
         if(event.getMessageChannel().getType() != ChannelType.TEXT)
             return;
 
-        List<MessageEmbed> embeds = new ArrayList<>();
-        embeds.add(new EmbedBuilder()
-            .setColor(Constants.COLOUR_YELLOW)
-            .setUrl(Constants.WEBSITE_URL)
-            .setImage("https://i.imgur.com/TNvKRIi.png")
-            .build());
-        embeds.add(new EmbedBuilder()
-            .setColor(Constants.COLOUR_YELLOW)
-            .setUrl(Constants.WEBSITE_URL)
-            .setImage("https://i.imgur.com/t1GJNnZ.png")
-            .build());
-
-        var rules = """
-            **:family:  ` Keep It Safe `**
-            > This is a server of all ages, and to keep it safe environment we ask for all interactions, this includes but not limited to posting messages and images, to be appropriate and respectful. Derogatory language, hate speech, slurs, sensitive topics, and adult content is strictly prohibited and will be punished accordingly.
-            
-            **:thumbs_up:  ` Use Common Sense `**
-            > We think this is pretty obvious but no one appreciates spam, typing in all caps, or blatant self promotion; keep that out of this server. There is also no need to ping members unnecessarily, this includes MrCrayfish and moderators.
-            
-            **:lock:  ` Keep On Topic `**
-            > This server is a Minecraft community about modding, and this means conversations should be related to Minecraft and modding. We aren't going to do anything if conversation goes slightly off topic, but we do ask to take them elsewhere if they don't fit into any of our text channel topics.
-            
-            **:man_construction_worker:️  ` Respect the Team `**
-            > The moderators of this server are here to help keep the server safe, they are not obligated to answer your questions related to Minecraft, be that game crashes or bugs. Pinging moderators should be reserved for serious rule violations.
-            
-            **:notebook_with_decorative_cover:  ` Discord TOS `**
-            > We adhere by the Discord Terms of Service and Community Guidelines, as such we will take action against anyone who does not. You can read them here:
-            > *Terms of Service:* https://discord.com/terms
-            > *Community Guidelines:* https://discord.com/guidelines
-            """;
-        embeds.add(new EmbedBuilder()
-            .setTitle("MrCrayfish's Discord Server Rules")
-            .setDescription(rules)
-            .setColor(Constants.COLOUR_YELLOW)
-            .build());
-
-        event.getMessageChannel().sendMessageEmbeds(embeds).queue();
-        event.reply("Success!").setEphemeral(true).queue();
+        Container container = Container.of(
+            Section.of(
+                Thumbnail.fromUrl("https://media.forgecdn.net/avatars/thumbnails/934/904/64/64/638411339753659456.png"),
+                TextDisplay.of("# MrCrayfish's Discord Server Rules")
+            ),
+            Separator.createDivider(Separator.Spacing.LARGE),
+            TextDisplay.of("**:family:  ` Keep It Safe `**"),
+            TextDisplay.of("This is a server of all ages, and to keep it safe environment we ask for all interactions, this includes but not limited to posting messages and images, to be appropriate and respectful. Derogatory language, hate speech, slurs, sensitive topics, and adult content is strictly prohibited and will be punished accordingly."),
+            Separator.createDivider(Separator.Spacing.SMALL),
+            TextDisplay.of("**:thumbs_up:  ` Use Common Sense `**"),
+            TextDisplay.of("We think this is pretty obvious but no one appreciates spam, typing in all caps, or blatant self promotion; keep that out of this server. There is also no need to ping members unnecessarily, this includes MrCrayfish and moderators."),
+            Separator.createDivider(Separator.Spacing.SMALL),
+            TextDisplay.of("**:lock:  ` Keep On Topic `**"),
+            TextDisplay.of("This server is a Minecraft community about modding, and this means conversations should be related to Minecraft and modding. We aren't going to do anything if conversation goes slightly off topic, but we do ask to take them elsewhere if they don't fit into any of our text channel topics."),
+            Separator.createDivider(Separator.Spacing.SMALL),
+            TextDisplay.of("**:man_construction_worker:️  ` Respect the Team `**"),
+            TextDisplay.of("The moderators of this server are here to help keep the server safe, they are not obligated to answer your questions related to Minecraft, be that game crashes or bugs. Pinging moderators should be reserved for serious rule violations."),
+            Separator.createDivider(Separator.Spacing.SMALL),
+            TextDisplay.of("**:notebook_with_decorative_cover:  ` Discord TOS `**"),
+            TextDisplay.of("We adhere by the Discord Terms of Service and Community Guidelines, as such we will take action against anyone who does not."),
+            TextDisplay.of("*Terms of Service:* https://discord.com/terms"),
+            TextDisplay.of("*Community Guidelines:* https://discord.com/guidelines")
+        ).withAccentColor(Constants.COLOUR_YELLOW);
+        event.getMessageChannel().sendMessageComponents(container).useComponentsV2().queue();
     }
 }
