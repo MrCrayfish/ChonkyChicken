ac5e3c66e994e7772cccfd81dd87dada6a20b9ba
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-08-07 19:35:18 +0930

    📦 Also check for edited messages

--- src/main/java/com/mrcrayfish/chonkybot/modules/DeleteEveryoneAndHereMention.java
+++ src/main/java/com/mrcrayfish/chonkybot/modules/DeleteEveryoneAndHereMention.java
@@ -4,21 +4,34 @@ import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.hooks.SubscribeEvent;
+
+import java.util.concurrent.TimeUnit;
 
 public class DeleteEveryoneAndHereMention
 {
     @SubscribeEvent
-    public static void onReceivedMessage(MessageReceivedEvent event)
+    private static void onMessageReceived(MessageReceivedEvent event)
     {
-        Message msg = event.getMessage();
+        runMentionCheck(event.getMessage(), event.getMember());
+    }
+
+    @SubscribeEvent
+    private static void onMessageUpdated(MessageUpdateEvent event)
+    {
+        runMentionCheck(event.getMessage(), event.getMember());
+    }
+
+    private static void runMentionCheck(Message msg, Member sender)
+    {
         if(msg.getContentRaw().contains("@everyone") || msg.getContentRaw().contains("@here"))
         {
-            Member member = event.getMember();
-            if(member != null && !member.hasPermission(msg.getGuildChannel(), Permission.MESSAGE_MENTION_EVERYONE))
+            if(sender != null && !sender.hasPermission(msg.getGuildChannel(), Permission.MESSAGE_MENTION_EVERYONE))
             {
                 // TODO instead send message to a moderation channel with special buttons to ban, kick, timeout, etc
                 msg.delete().reason("@everyone and @here mention is not allowed").queue();
+                sender.timeoutFor(60, TimeUnit.SECONDS).queue();
             }
         }
     }
