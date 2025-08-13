3e486a457ab2ad93607755b1b919d23d5fe04517
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-08-14 03:27:21 +0930

    Update Emoji.java

--- src/main/java/com/mrcrayfish/chonky_bot/Emoji.java
+++ src/main/java/com/mrcrayfish/chonky_bot/Emoji.java
@@ -2,8 +2,8 @@ package com.mrcrayfish.chonky_bot;
 
 public class Emoji
 {
-    public static final String OFFICIAL_DOWNLOAD = "<:700037139240321026:1404814023274266644>";
-    public static final String CURSEFORGE = "<:1341800402865360987:1404811158585872414>";
-    public static final String MODRINTH = "<:1040805511538421890:1404814885132701706>";
-    public static final String INFO = "<:1272083267440607244:1404821990052724746>";
+    public static final String OFFICIAL_DOWNLOAD = "<:mrcrayfish_website:1405248756722438194>";
+    public static final String CURSEFORGE = "<:curseforge:1405248704482381926>";
+    public static final String MODRINTH = "<:modrinth:1405248304555757568>";
+    public static final String INFO = "<:info:1405248684349722654>";
 }
